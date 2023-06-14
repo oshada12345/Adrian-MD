0227b2f87e3fa9a1383d5123043484993f9ba915
oshadaXD <135181748+oshada12345@users.noreply.github.com>
2023-06-14 16:25:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ global.author = 'Fuck You Bitch\nYEEEEEE'
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² ini!
-global.sessionName = 'session'
+global.sessionName = 'session-1'
 
 //—————「 Set Prefix 」—————//
 //gausah di apa² ini!
